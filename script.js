35e990adda0fd75515c9622ed9028c7dfd026d9d
schneiderdauinheimeringrid-pixel <schneiderdauinheimeringrid@gmail.com>
2025-11-10 17:24:01 -0300

    catalogo-pt3

---
+++ script.js
@@ -0,0 +1,66 @@
+const CATALOG_ITEMS = [
+    {
+        id: 1,
+        titulo: "O mistério da Floresta Negra",
+        categoria: "Livros",
+        detalhes: "Um romance policiais envolvente que se passa nas profundezas da Floresta Negra",
+        preco: "R$ 49,90",
+        estoque: 15,
+        autor: "Ana Clara Silva",
+        lancamento: "2024"
+    },
+    {
+          id: 2,
+        titulo: "Vaso de cerâmica Rústica",
+        categoria: "Artesanato",
+        detalhes: "Vaso decorativo, feito e pintado a mão, ideal para flores secas ou como peça central em mesas. Cada peça única. Cor roxa vibrante com detalhes de ouro velho",
+        preco: "R$ 120,00",
+        estoque: 3,
+        material: "Argila Queimada e Tinta Acrílica",
+        dimensoes: "20cm x 15cm"
+    },
+    {
+           id: 3,
+        titulo: "Crônicas de Marte",
+        categoria: "Livros",
+        detalhes: "Clássicos da ficção científica que explora a colonização humana em Marte e seua dilemas éticos. Uma leitura obrigatória para fãs de gênero.",
+        preco: "R$ 35,50",
+        estoque: 22,
+        autor: "Roberto Almeida",
+        lancamento: "1998 (Edição comemorativa)"
+    },
+    {
+   id: 4,
+        titulo: "Colar de Semente Natural",
+        categoria: "Artesanato",
+        detalhes: "Colar sustentável feito com sementes de açaí e tucumã. Perfeito para um visual boêmio e natural. Fecho ajustável. ",
+        preco: "R$75,90",
+        estoque: 8,
+        material: "Semente Naturais e Fio Encerada",
+        comprimento: "50cm"
+    }
+];
+/**
+ * Adiciona listeners aos botões "Ver Destalhes" para popular o modal dinamicamente.
+ */
+const modalElement= document.querySelector('#detalheModal');
+const modalTitle= modalElement.querySelector('.modal-title');
+const modalBody= modalElement.querySelector('.modal-body');
+
+// 1. Ouvinte para popular o modal ANTES de ser exibido
+modalElement.addEventListener('show.bs.modal', function (event){
+    const button = event.relatedTarget;
+    const itemID = parseInt(button.getAttribute('data-item-id'));
+    const item = CATALOG_ITEMS.find(i=> i.id === itemID);
+
+    if (item) {
+        modalTitle.textContent = item.titulo;
+    }
+
+    let detailsHTML = `
+        <p class="mb-1"><strong>Categoria:</strong> <span class="badge bg-secundary">${item.categoria}</span></p>
+        <p class="fs-4 fw-bold text-success mb-3>Preço: ${item.preco}</p>
+        <hr>
+        <p>${item.detalhes}</p>
+    `;
+});
